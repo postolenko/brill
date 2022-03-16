2a3069df20d04a316799710c4b24480f3cae98f6
postolenko <denis.postolenko@gmail.com>
2022-03-16 18:47:38 +0200

    Slider

---
+++ README.md
@@ -0,0 +1,1 @@
+# http://postolenko.github.io/slider/index.html
---
+++ js/scripts.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+
+    if( $(".br_slider_1").length > 0 ) {
+        $(".br_slider_1").not(".slick-initialized").slick({
+            dots: false,
+            arrows: true,
+            autoplay: true,
+            autoplaySpeed: 4000,
+            speed: 1200,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                  breakpoint: 900,
+                  settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                  }
+                },
+                {
+                  breakpoint: 570,
+                  settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                  }
+                }
+              ]
+        });
+    }
+
+});
