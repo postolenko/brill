6511bb377f7adb2502b68c5ed3257df72c5d82d3
postolenko <denis.postolenko@gmail.com>
2022-03-17 12:23:28 +0200

    Slider

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,27 @@
+function getMarginBootmSlide() {
+  $(".br_slider_2").each(function() {
+    $(this).find(".br_slide_2").css({
+      "margin-bottom" : "auto"
+    });
+    hArr = [];
+    desc = $(this).find(".br_slide_2_descript");
+    desc.each(function() {
+      h = $(this).outerHeight();
+      hArr.push(h);
+    });
+    maxHeight = Math.max.apply(null, hArr);
+    $(this).find(".br_slide_2").css({
+      "margin-bottom" : maxHeight + "px"
+    });
+  });
+}
+$(window).load(function() {
+  getMarginBootmSlide();
+});
+$(window).resize(function() {
+  getMarginBootmSlide();
+});
 $(document).ready(function() {
-
     if( $(".br_slider_1").length > 0 ) {
         $(".br_slider_1").not(".slick-initialized").slick({
             dots: false,
@@ -27,5 +49,38 @@ $(document).ready(function() {
               ]
         });
     }
-
+    if( $(".br_slider_2").length > 0 ) {
+      $(".br_slider_2").not(".slick-initialized").slick({
+          dots: false,
+          arrows: true,
+          autoplay: true,
+          autoplaySpeed: 4000,
+          speed: 1200,
+          slidesToShow: 4,
+          slidesToScroll: 2,
+          responsive: [
+              {
+                breakpoint: 1072,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 900,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2
+                }
+              },
+              {
+                breakpoint: 570,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+      });
+    }
 });
